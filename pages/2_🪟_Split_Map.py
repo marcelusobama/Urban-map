2cb5cfb5dca18039babdad2c4547e43d913c8e37
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-01 18:05:11 -0300

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -3,20 +3,11 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
-"""
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+st.title("Comparação de Mapas")
 
-st.title("Split-panel Map")
-
-with st.expander("See source code"):
-    with st.echo():
+#with st.expander("See source code"):
+    #with st.echo():
         m = leafmap.Map()
         m.split_map(
             left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
