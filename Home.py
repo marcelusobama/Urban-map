fd231f83c8b3139d21bd031c2e0ae850d16f65f0
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-01 17:40:14 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,13 +25,7 @@ st.markdown(
 
 st.header("Instructions")
 
-markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
 
-"""
 
 st.markdown(markdown)
 
