48bbb726724a5088530da80fcf75278bf733491a
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-01 17:46:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,21 +17,6 @@ st.sidebar.image(logo)
 # Customize page title
 st.title("Streamlit for Geospatial Applications")
 
-st.markdown(
-    """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
-    """
-)
-
-st.header("Instructions")
-
-markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
-"""
 
 st.markdown(markdown)
 
