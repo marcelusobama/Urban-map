0ac5228e4323324644fc32f01379dfdcd346530e
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 10:31:38 -0300

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -12,6 +12,14 @@ with st.expander("See source code"):
         m.split_map(
             left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
         )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
+        legend_dict = {
+    'Floresta Tropical': '#006400',
+    'Pastagem': '#7FFF00',
+    'Terras Cultivadas': '#FFD700',
+    # Adicione outras categorias e cores conforme necessário
+}
+
+m.add_legend(title='Cobertura da Terra ESA', legend_dict=legend_dict)
+        
 
 m.to_streamlit(height=700)
