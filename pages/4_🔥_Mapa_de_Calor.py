af0c0fd3ba9b021a1f84c66a1ab183cd1f2b46fc
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 16:01:04 -0300

    Update 4_🔥_Mapa_de_Calor.py

--- pages/4_🔥_Mapa_de_Calor.py
+++ pages/4_🔥_Mapa_de_Calor.py
@@ -8,7 +8,7 @@ st.title("Mapa de Calor")
 
 with st.expander("See source code"):
     with st.echo():
-       filepath = "https://raw.githubusercontent.com/marcelusobama/Urban-map/master/br_cidades.csv"
+       filepath = "https://raw.githubusercontent.com/marcelusobama/Urban-map/b34574e4a9b258362549be23824c0815dea10b0d/MunicipiosBrasil.csv"
 m = leafmap.Map(center=[-15, -48], zoom=4)
 m.add_heatmap(
     filepath,
