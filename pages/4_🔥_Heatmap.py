bf9a55de301608a096c679373180763f92070932
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 13:41:04 -0300

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -3,22 +3,13 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
 
 st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
+        filepath = "https://github.com/marcelusobama/Urban-map/blob/master/br_cidades.csv"
+        m = leafmap.Map(center=[-15, -48], zoom=4, tiles="stamentoner")
         m.add_heatmap(
             filepath,
             latitude="latitude",
