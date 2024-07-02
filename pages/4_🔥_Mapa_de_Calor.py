6e6d089309cd4a829edf0976f1806dedb8225c5a
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 16:23:07 -0300

    Update 4_🔥_Mapa_de_Calor.py

--- pages/4_🔥_Mapa_de_Calor.py
+++ pages/4_🔥_Mapa_de_Calor.py
@@ -1,5 +1,19 @@
 import streamlit as st
-import leafmap.leafmap as leafmap
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
