53b014f8ab2e9ea78965f6391b05318c751a5651
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 16:22:26 -0300

    Update 4_🔥_Mapa_de_Calor.py

--- pages/4_🔥_Mapa_de_Calor.py
+++ pages/4_🔥_Mapa_de_Calor.py
@@ -1,23 +1,16 @@
 import streamlit as st
 import leafmap.leafmap as leafmap
 
-# Modificação do endereço do link
-filepath = "https://github.com/marcelusobama/Urban-map/blob/master/MunicipiosBrasil.csv"
-
 with st.expander("See source code"):
     with st.echo():
-        # Inicialização do mapa com centro e zoom definidos
-        m = leafmap.Map(center=[-14.235, -51.9253], zoom=4)  # Ajustando o centro para o Brasil
-
-        # Adicionando o heatmap ao mapa
+        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        m = leafmap.Map(center=[40, -100], zoom=4)
         m.add_heatmap(
             filepath,
             latitude="latitude",
             longitude="longitude",
-            value="region",  # Ajuste conforme os dados no CSV
+            value="pop_max",
             name="Heat map",
             radius=20,
         )
-
-        # Exibindo o mapa no Streamlit
-        m.to_streamlit(height=700)
+m.to_streamlit(height=700)
