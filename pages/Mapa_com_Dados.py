030bd4c9bc9c208982b83ed9c307e6bf94da28ee
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 21:35:23 -0300

    Update Mapa_com_Dados.py

--- pages/Mapa_com_Dados.py
+++ pages/Mapa_com_Dados.py
@@ -4,7 +4,7 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 st.title("Mapa com Dados")
 m = leafmap.Map(google_map="HYBRID",center=[-15.83, -47.86], zoom=10)
-in_geojson = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/cable_geo.geojson"
+in_geojson = "https://ide.geobases.es.gov.br/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typename=geonode%3AAREA_IMOVEL_1&outputFormat=json&srs=EPSG%3A31984&srsName=EPSG%3A31984"
 m.add_geojson(in_geojson, layer_name="Cable lines")
 
 m.to_streamlit(height=700)
