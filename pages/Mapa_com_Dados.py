5ad4dcc951975a7dca1da9d78caa7a7e68ae7d27
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 21:26:39 -0300

    Update Mapa_com_Dados.py

--- pages/Mapa_com_Dados.py
+++ pages/Mapa_com_Dados.py
@@ -13,4 +13,4 @@ m = leafmap.Map(center=[0, 0], zoom=2)
 in_geojson = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/cable_geo.geojson"
 m.add_geojson(in_geojson, layer_name="Cable lines")
 
-m
+m.to_streamlit(height=700)
