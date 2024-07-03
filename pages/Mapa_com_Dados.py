8a8906e175666c5e8776c1eefbed7881ee183ad2
Marcelus oliveira de jesus <marcelusambiental@gmail.com>
2024-07-02 21:32:24 -0300

    Update Mapa_com_Dados.py

--- pages/Mapa_com_Dados.py
+++ pages/Mapa_com_Dados.py
@@ -3,7 +3,7 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 st.title("Mapa com Dados")
-m = leafmap.Map(center=[-15.83, -47.86], zoom=10)
+m = leafmap.Map(google_map="HYBRID",center=[-15.83, -47.86], zoom=10)
 in_geojson = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/cable_geo.geojson"
 m.add_geojson(in_geojson, layer_name="Cable lines")
 
